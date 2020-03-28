8c08d34e8a07f5a91dfae6be2d2ba31d54e1a0de
Fan Yang <yangfcm16@gmail.com>
2020-03-28 15:01:45 +1030

    greedy pig: can input number

--- src/components/lessons/greedy-pig/ChangeKiller.vue
+++ src/components/lessons/greedy-pig/ChangeKiller.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="container mt-3">
     <h3 class="text-success text-center">Change the killer dice</h3>
-    <div class="d-flex justify-content-center mt-5">
+    <div
+      class="d-flex justify-content-center mt-5"
+      :style="{ visibility: myKillerDice>=1 && myKillerDice <=6 ? 'visible' : 'hidden' }"
+      autofocus
+    >
       <app-dice :number="myKillerDice" :index="0"></app-dice>
     </div>
     <div class="d-flex justify-content-center mt-3">
       <input
         type="text"
         class="form-control text-center"
-        style="width: 100px; background: #efefef;"
+        style="width: 100px; "
         v-model.number="myKillerDice"
         @keypress="handleCheckInput"
-        disabled
       />
     </div>
-    <div class="text-center text-danger my-3">
-      Enter the killer dice number(1-6)
-    </div>
+    <div class="text-center text-danger my-3">Enter the killer dice number(1-6)</div>
     <div class="text-center">
-      <button class="btn btn-outline-success" @click="handleSetKiller">
-        OK
-      </button>
+      <button class="btn btn-outline-success" @click="handleSetKiller" :disabled="!validDice">OK</button>
     </div>
   </div>
 </template>
@@ -37,12 +36,21 @@ export default {
       myKillerDice: this.killerDice
     };
   },
+  computed: {
+    validDice() {
+      return (
+        typeof this.myKillerDice === "number" &&
+        this.myKillerDice >= 1 &&
+        this.myKillerDice <= 6
+      );
+    }
+  },
   methods: {
     handleCheckInput(e) {
       if (e.charCode < 49 || e.charCode > 54) {
         e.preventDefault();
       }
-      if (e.target.value.length != 0) {
+      if (e.target.value.length > 1) {
         e.preventDefault();
       }
       this.myKillerDice = e.target.value;
@@ -52,11 +60,11 @@ export default {
     }
   },
   mounted() {
-    document.addEventListener("keypress", e => {
-      if (e.charCode >= 49 && e.charCode <= 54) {
-        this.myKillerDice = Number(e.key);
-      }
-    });
+    // document.addEventListener("keypress", e => {
+    //   if (e.charCode >= 49 && e.charCode <= 54) {
+    //     this.myKillerDice = Number(e.key);
+    //   }
+    // });
   }
 };
 </script>
