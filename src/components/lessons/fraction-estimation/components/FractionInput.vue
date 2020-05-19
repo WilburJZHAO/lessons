3ab5c9ebe33b87e9bc4c3da2b39ff7ab6bb46347
Fan Yang <yangfcm16@gmail.com>
2020-05-19 20:35:07 +0930

    fraction estimation: make fraction display vertical

--- src/components/lessons/fraction-estimation/components/FractionInput.vue
+++ src/components/lessons/fraction-estimation/components/FractionInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="d-flex align-items-center justify-content-center" v-if="inputType===1">
+    <div class="d-flex align-items-center justify-content-center flex-column" v-if="inputType===1">
       <div>
         <input
           type="number"
@@ -10,12 +10,12 @@
           style="max-width: 90px;"
         />
       </div>
-      <h5 class="mx-2">/</h5>
+      <div style=" width: 30%; border-bottom: 1px solid #333;" class="mt-3 mb-2"></div>
       <h5>{{ fractionData.denominator}}</h5>
     </div>
-    <div class="d-flex align-items-center justify-content-center" v-if="inputType===2">
+    <div class="d-flex flex-column align-items-center justify-content-center" v-if="inputType===2">
       <h5>{{ fractionData.numerator}}</h5>
-      <h5 class="mx-2">/</h5>
+      <div style=" width: 30%; border-bottom: 1px solid #333;" class="mt-2 mb-3"></div>
       <input
         type="number"
         class="form-control mr-2"
